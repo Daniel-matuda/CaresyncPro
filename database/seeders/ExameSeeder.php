7e3eff93ebf2260579c7b3b49dde3c6b1d9b1fba
daniel-matuda <danielmatudaoficial@gmail.com>
2024-11-18 13:29:28 -0300

    limpando arq

--- database/seeders/ExameSeeder.php
+++ database/seeders/ExameSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class QueixaEDuracaoSeeder extends Seeder
+class ExameSeeder extends Seeder
 {
     /**
      * Run the database seeds.
